77bcaba02b2935b857f7adf57e2548470baaf906
ellie952 <ellie952@revature.net>
2025-10-06 13:29:04 -0500

    Create Post component

--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -1,6 +1,11 @@
+import Post from "../../components/Post/Post"
+
 function DashboardPage() {
     return (
-        <div>DashboardPage</div>
+        <div>
+            <h1>Dashboard</h1>
+            <Post />
+        </div>
     )
 }
 
