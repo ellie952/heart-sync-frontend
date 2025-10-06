60e641d5ceeadac8d8ff666c73850dd253380868
ellie952 <ellie952@revature.net>
2025-10-06 14:10:00 -0500

    Update NavBar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,39 +1,42 @@
 function NavBar() {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                        <a className="nav-link" href="/">
-                            Dashboard
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="/profile">
-                            Profile
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="/login">
-                            Login
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="/register">
-                            Register
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="/new-playlist">
-                            New Post
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="/settings">
-                            Settings
-                        </a>
-                    </li>
-                </ul>
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">HeartSync</a>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">
+                                Dashboard
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/profile">
+                                Profile
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/login">
+                                Login
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/register">
+                                Register
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/new-playlist">
+                                New Post
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/settings">
+                                Settings
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     )
