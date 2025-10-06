eedc68c231fbebcc528d1bd1f5991ef3bdcca4ed
ellie952 <ellie952@revature.net>
2025-10-06 13:38:09 -0500

    Create PostList component

--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -1,7 +1,10 @@
+import PostList from "../../components/PostList/PostList"
+
 function DashboardPage() {
     return (
         <div>
             <h1>Dashboard</h1>
+            <PostList />
         </div>
     )
 }
