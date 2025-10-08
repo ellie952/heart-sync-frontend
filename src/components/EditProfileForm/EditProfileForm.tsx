a407d4de51323086e90bd30b8f93177b9f812c75
ellie952 <ellie952@revature.net>
2025-10-08 14:56:01 -0500

    Begin connecting EditProfileForm.tsx to backend

--- src/components/EditProfileForm/EditProfileForm.tsx
+++ src/components/EditProfileForm/EditProfileForm.tsx
@@ -1,15 +1,61 @@
+import axios from "axios";
 import { useState, type ChangeEvent, type FormEvent } from "react";
+import { useNavigate } from "react-router";
 
 function EditProfileForm() {
     const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [hasError, setHasError] = useState(false);
+
+    const USER_API_BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/users`;
+
+    const navigate = useNavigate();
 
     function handleUsername(e: ChangeEvent<HTMLInputElement>) {
-        setUsername(e.target.value)
+        setUsername(e.target.value);
     }
 
-    function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    function handleEmail(e: ChangeEvent<HTMLInputElement>) {
+        setEmail(e.target.value);
+    }
+
+    async function handleSubmit(e: FormEvent<HTMLFormElement>) {
         e.preventDefault();
-        console.log("Profile updated");
+        setHasError(false);
+
+        const token = localStorage.getItem("TOKEN");
+        if (!token) {
+            setHasError(true);
+            console.error("You must be logged in to delete your profile.");
+            return;
+        }
+
+        if (username && email) {
+            try {
+                await axios.put(
+                    `${USER_API_BASE_URL}/update`, // endpoint doesn't exist, we'll need something like this or similar
+                    {
+                        username,
+                        email
+                    },
+                    {
+                        headers: {
+                            "Content-Type": "application/json",
+                            Authorization: `Bearer ${token}`,
+                        },
+                    }
+                );
+
+                navigate("/settings");
+            } catch (error: unknown) {
+                setHasError(true);
+                if (axios.isAxiosError(error)) {
+                    console.error("Error registering user:", error.response?.data || error.message);
+                } else {
+                    console.error("Unexpected error:", error);
+                }
+            }
+        }
     }
 
     return (
@@ -21,7 +67,19 @@ function EditProfileForm() {
                 onChange={handleUsername}
                 required
             />
+            <input
+                type="email"
+                placeholder="email"
+                value={email}
+                onChange={handleEmail}
+                required
+            />
             <input type="submit" />
+            {hasError && (
+                <p>
+                    Password reset failed. Please try again.
+                </p>
+            )}
         </form>
     )
 }
