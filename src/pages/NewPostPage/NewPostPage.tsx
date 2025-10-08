af27d80acb3067a9a1549085d34fd84fbfcffccc
brisasalazar <brisal.853@gmail.com>
2025-10-08 13:06:25 -0500

    new post componened

--- src/pages/NewPostPage/NewPostPage.tsx
+++ src/pages/NewPostPage/NewPostPage.tsx
@@ -1,10 +1,7 @@
-import NewPostForm from "../../components/NewPostForm/NewPostForm"
-
 function NewPostPage() {
     return (
         <div>
             <h1>New Post</h1>
-            <NewPostForm />
         </div>
     )
 }
