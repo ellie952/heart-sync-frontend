19a9893b830b6d6c224e31f2b6abade0a1cef278
ellie952 <ellie952@revature.net>
2025-10-08 15:39:44 -0500

    Complete connecting EditProfileForm.tsx to backend

--- src/components/EditProfileForm/EditProfileForm.tsx
+++ src/components/EditProfileForm/EditProfileForm.tsx
@@ -37,6 +37,7 @@ function EditProfileForm() {
         }
 
         getUserData()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     function handleNewUsername(e: ChangeEvent<HTMLInputElement>) {
