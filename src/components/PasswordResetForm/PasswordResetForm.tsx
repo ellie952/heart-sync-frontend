d69c920ec85fd91c969e2aeb799f1dbad54f1a80
ellie952 <ellie952@revature.net>
2025-10-08 14:01:16 -0500

    Connect ResetPasswordForm.tsx to backend

--- src/components/PasswordResetForm/PasswordResetForm.tsx
+++ src/components/PasswordResetForm/PasswordResetForm.tsx
@@ -1,16 +1,55 @@
+import axios from "axios";
 import { useState, type ChangeEvent, type FormEvent } from "react";
+import { useNavigate } from "react-router";
 
 function PasswordResetForm() {
     const [prevPassword, setPrevPassword] = useState("");
     const [newPassword, setNewPassword] = useState("");
     const [confirmedPassword, setConfirmedPassword] = useState("");
+    const [hasError, setHasError] = useState(false);
 
-    function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    const USER_API_BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/users`;
+
+    const navigate = useNavigate();
+
+    async function handleSubmit(e: FormEvent<HTMLFormElement>) {
         e.preventDefault();
+        setHasError(false);
+
+        const token = localStorage.getItem("TOKEN");
+        if (!token) {
+            setHasError(true);
+            console.error("You must be logged in to delete your profile.");
+            return;
+        }
 
         if ((newPassword === confirmedPassword) && (newPassword !== prevPassword)) {
-            console.log("Password reset successfully.");
+            try {
+                await axios.put(
+                    `${USER_API_BASE_URL}/password`,
+                    {
+                        oldPassword: prevPassword,
+                        newPassword: confirmedPassword
+                    },
+                    {
+                        headers: {
+                            "Content-Type": "application/json",
+                            Authorization: `Bearer ${token}`,
+                        },
+                    }
+                );
+
+                navigate("/login")
+            } catch (error: unknown) {
+                setHasError(true);
+                if (axios.isAxiosError(error)) {
+                    console.error("Error registering user:", error.response?.data || error.message);
+                } else {
+                    console.error("Unexpected error:", error);
+                }
+            }
         } else {
+            setHasError(true);
             console.error("Password reset failed.");
         }
     }
@@ -51,6 +90,11 @@ function PasswordResetForm() {
                 required
             />
             <input type="submit" />
+            {hasError && (
+                <p>
+                    Password reset failed. Please try again.
+                </p>
+            )}
         </form>
     )
 }
