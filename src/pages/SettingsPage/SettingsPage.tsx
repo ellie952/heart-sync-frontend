c45011e4e0da8950af7a8c9671d3617ae82a3c33
ellie952 <ellie952@revature.net>
2025-10-13 19:08:35 -0500

    SettingsPage tests

--- src/pages/SettingsPage/SettingsPage.tsx
+++ src/pages/SettingsPage/SettingsPage.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router"
 function SettingsPage() {
     return (
         <div>
-            <h1>Settings</h1>
+            <h1 data-test="title">Settings</h1>
             <Outlet />
         </div>
     )
