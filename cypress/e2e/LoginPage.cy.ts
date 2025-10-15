774e992f2e540266d098fe5bcffea849b21ebed3
nnamdionyeije <81874532+nnamdionyeije@users.noreply.github.com>
2025-10-15 12:07:26 -0400

    Cypress tests for the login and registration pages

--- cypress/e2e/LoginPage.cy.ts
+++ cypress/e2e/LoginPage.cy.ts
@@ -1,8 +1,72 @@
 describe('LoginPage tests', () => {
-  it('Contains correct header', () => {
+  
+  beforeEach(() => {
     cy.visit(Cypress.env("FRONTEND_BASE_URL") + "/login");
+  })
+  
+  it('Contains correct header', () => {
+    
     cy.get("[data-test='title']").should(
       "contain.text", "Login"
     );
+    cy.get("[href='/login']").should(
+      "contain.text", "Login"
+    );
+    cy.get("[href='/register']").should(
+      "contain.text", "Register"
+    );
   });
+
+  it('contains correct fields', () => {
+    cy.get("[placeholder='Username']").should(
+      "contain.text", ""
+    );
+    cy.get("[placeholder='Password']").should(
+      "contain.text", ""
+    );
+    cy.get("[type='submit']").should(
+      "contain.text", ""
+    );
+  })
+
+  it('should allow logging in with correct credentials', () => {
+    cy.get("[placeholder='Username']").type(
+      'user1'
+    );
+    cy.get("[placeholder='Password']").type(
+      'pass1{enter}'
+    );
+    cy.get("[data-test='title']").should(
+      "contain.text", "Dashboard"
+    )
+  })
+
+  it('should reject a login with incorrect credentials', () => {
+    cy.get("[placeholder='Username']").type(
+      'user10'
+    );
+    cy.get("[placeholder='Password']").type(
+      'pass10{enter}'
+    );
+    cy.get("p").should(
+      "contain.text", "Login failed:"
+    )
+  })
+
+  it('should not allow a logged in user to log in again', () => {
+    cy.get("[placeholder='Username']").type(
+      'user1'
+    );
+    cy.get("[placeholder='Password']").type(
+      'pass1{enter}'
+    );
+    cy.get("[data-test='title']").should(
+      "contain.text", "Dashboard"
+    )
+    cy.visit(Cypress.env("FRONTEND_BASE_URL") + "/login");
+    cy.get("p").should(
+      "contain.text", "You are already logged in."
+    )
+  })
+
 });
--- cypress/e2e/RegisterPage.cy.ts
+++ cypress/e2e/RegisterPage.cy.ts
@@ -1,8 +1,66 @@
 describe('RegisterPage tests', () => {
-  it('Contains correct header', () => {
+  const suffix = Math.random();
+  // in order to generate unique username and emails that don't
+  // conflict with the existing database
+
+  beforeEach(() => {
     cy.visit(Cypress.env("FRONTEND_BASE_URL") + "/register");
+    
+  })
+
+  it('Contains correct headers', () => {
     cy.get("[data-test='title']").should(
       "contain.text", "Register"
     );
+    cy.get("[href='/login']").should(
+      "contain.text", "Login"
+    );
+    cy.get("[href='/register']").should(
+      "contain.text", "Register"
+    );
   });
+
+  it('contains correct fields', () => {
+    
+
+    cy.get("[placeholder='Username']").should("be.empty");
+    cy.get("[placeholder='Email']").should("be.empty");
+    cy.get("[placeholder='Password']").should("be.empty");
+    cy.get("[placeholder='Confirm password']").should("be.empty");
+    cy.get("[type='submit']").should("be.empty");
+  })
+
+  it('should reject a registration with missing credentials', () => {
+    cy.get("[placeholder='Username']").type('user9559');
+    cy.get("[placeholder='Email']").type('user9559@revature.net');
+    cy.get("[placeholder='Confirm password']").type('pass1{enter}');
+
+    cy.get("p").should(
+      "contain.text", "Registration failed:"
+    )
+  })
+
+  it('should allow logging in with correct credentials', () => {
+    cy.get("[placeholder='Username']").type(`user${suffix}`);
+    cy.get("[placeholder='Email']").type(`user${suffix}@revature.net`);
+    cy.get("[placeholder='Password']").type('pass1');
+    cy.get("[placeholder='Confirm password']").type('pass1{enter}');
+
+    cy.get("[data-test='title']").should(
+      "contain.text", "Login"
+    );
+  })
+
+  it('should reject a registration with already used credentials', () => {
+    cy.get("[placeholder='Username']").type(`user${suffix}`);
+    cy.get("[placeholder='Email']").type(`user${suffix}@revature.net`);
+    cy.get("[placeholder='Password']").type('pass1');
+    cy.get("[placeholder='Confirm password']").type('pass1{enter}');
+
+    cy.get("p").should(
+      "contain.text", "Registration failed:"
+    )
+  })
+
+
 });
